cb23077d837e842887edf387e9e6b900af729a2e
Steve Sargent <gs_sarge@yahoo.com>
2025-06-06 18:11:22 -0700

    Added custom openapi configurations to booking-service and event-service components

--- event-service-common/src/main/java/piper1970/eventservice/common/events/dto/EventDto.java
+++ event-service-common/src/main/java/piper1970/eventservice/common/events/dto/EventDto.java
@@ -1,6 +1,7 @@
 package piper1970.eventservice.common.events.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,18 +17,54 @@ import piper1970.eventservice.common.events.status.EventStatus;
 @NoArgsConstructor
 @ToString
 @With
+@Schema(description = "Event")
 public class EventDto {
 
   @ToString.Exclude
+  @Schema(
+      description = "Event ID"
+  )
   private Integer id;
+
+  @Schema(
+      description = "Creator of event"
+  )
   private String facilitator;
+
+  @Schema(
+      description = "Event Title"
+  )
   private String title;
+
+  @Schema(
+      description = "Short description of event"
+  )
   private String description;
+
+  @Schema(
+      description = "Location of event"
+  )
   private String location;
+
+  @Schema(
+      description = "status of event",
+      allowableValues = {"AWAITING", "IN_PROGRESS", "CANCELLED", "COMPLETED"}
+  )
   private EventStatus eventStatus;
 
+  @Schema(
+      description = "date and time of event"
+  )
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
   private LocalDateTime eventDateTime;
+
+  @Schema(
+      description = "duration of event, in minutes"
+  )
   private Integer durationInMinutes;
+
+  @Schema(
+      description = "number of slots available for booking"
+  )
   private Integer availableBookings;
 }
--- event-service/src/main/java/piper1970/eventservice/dto/model/EventCreateRequest.java
+++ event-service/src/main/java/piper1970/eventservice/dto/model/EventCreateRequest.java
@@ -1,5 +1,6 @@
 package piper1970.eventservice.dto.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -23,31 +24,56 @@ import piper1970.eventservice.common.validation.annotations.NullOrNotBlank;
 @NoArgsConstructor
 @ToString
 @With
+@Schema(description = "Create Event Request")
 public class EventCreateRequest {
 
+  @Schema(
+      description = "Creator of event, taken from security token",
+      accessMode = Schema.AccessMode.READ_ONLY,
+      nullable = true
+  )
   @Null(message = "[facilitator] cannot be set within JSON. It is injected from authentication token")
   private String facilitator;
 
+  @Schema(
+      description = "Event Title"
+  )
   @NotBlank(message = "[title] field cannot be empty")
   @Size(max = 255, message = "[title] field must be within 255 character range")
   private String title;
 
+  @Schema(
+      description = "Short description of event",
+      nullable = true
+  )
   @NullOrNotBlank(message = "[description] field cannot be blank, but may be omitted")
   @Size(max = 255, message = "[description] field must be within 255 character range")
   private String description;
 
+  @Schema(
+      description = "Location of event"
+  )
   @NotBlank(message = "[location] field cannot be empty")
   @Size(max = 255, message = "[location] field must be within 255 character range")
   private String location;
 
+  @Schema(
+      description = "date and time of event"
+  )
   @NotNull(message = "[eventDateTime] field must be present and non-null")
   @CustomFuture(message = "[eventDateTime] field must be a future date")
   private LocalDateTime eventDateTime;
 
+  @Schema(
+      description = "duration of event, in minutes"
+  )
   @NotNull(message = "[durationInMinutes] field must be present and non-null")
   @Min(value = 30, message = "[durationInMinutes] field must be at least 30 minutes")
   private Integer durationInMinutes;
 
+  @Schema(
+      description = "number of slots available for booking"
+  )
   @NotNull(message = "[availableBookings] field must be present and non-null")
   @PositiveOrZero(message = "[availableBookings] field cannot be negative")
   @Max(value=32767) // smallint
