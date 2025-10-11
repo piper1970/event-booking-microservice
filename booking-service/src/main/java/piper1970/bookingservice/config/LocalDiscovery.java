5bf1e8a68c24d323c9fe3d906c7a8898af3c4c85
Steve Sargent <gs_sarge@yahoo.com>
2025-10-10 17:24:27 -0700

    Setup SSL profile for compose
    Setup ssl_compose profile, along with updated compose file.
    
    Does not work at the moment, either in Postgres or Swagger.
    
    ssl_local profile, which runs spring microservices locally against docker environment works both in Swagger and Postman.
    
    Pushing up for now.  I believe these problems are due to http/https boundary issues.

--- booking-service/src/main/java/piper1970/bookingservice/config/LocalDiscovery.java
+++ booking-service/src/main/java/piper1970/bookingservice/config/LocalDiscovery.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile({"local_discovery", "compose",  "ssl_local"})
+@Profile({"local_discovery", "compose",  "ssl_local", "ssl_compose"})
 @Configuration
 @EnableDiscoveryClient
 public class LocalDiscovery {
